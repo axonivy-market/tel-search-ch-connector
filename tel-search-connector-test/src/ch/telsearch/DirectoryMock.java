55df99c8cb908373f96d1a7569430ae52c32498f
Louis Müller <louis.mueller@axonivy.com>
2021-11-15 15:54:59 +0100

    MARKET-135 added test for API key feature

--- tel-search-connector-test/src/ch/telsearch/DirectoryMock.java
+++ tel-search-connector-test/src/ch/telsearch/DirectoryMock.java
@@ -8,7 +8,9 @@ import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
 
@@ -17,25 +19,23 @@ import io.swagger.v3.oas.annotations.Hidden;
 @Path("telMock")
 @PermitAll
 @Hidden
-public class DirectoryMock
-{
+public class DirectoryMock {
+  public static final String MY_KEY = "myKey";
+
   @GET
   @Produces(MediaType.APPLICATION_ATOM_XML)
-  public String was()
-  {
-    return load("directory.xml");
+  public Response was(@QueryParam("key") String key) {
+    return Response.status(200)
+            .entity(load("directory.xml"))
+            .header(MY_KEY, key)
+            .build();
   }
 
-
-  private static String load(String path)
-  {
-    try(InputStream is = DirectoryMock.class.getResourceAsStream(path))
-    {
+  private static String load(String path) {
+    try (InputStream is = DirectoryMock.class.getResourceAsStream(path)) {
       return IOUtils.toString(is, StandardCharsets.UTF_8);
-    }
-    catch (IOException ex)
-    {
-      throw new RuntimeException("Failed to read resource: "+path);
+    } catch (IOException ex) {
+      throw new RuntimeException("Failed to read resource: " + path);
     }
   }
 }
--- tel-search-connector-test/src_test/com/axonivy/connector/telsearch/test/PhoneDirectoryTest.java
+++ tel-search-connector-test/src_test/com/axonivy/connector/telsearch/test/PhoneDirectoryTest.java
@@ -2,6 +2,10 @@ package com.axonivy.connector.telsearch.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.connector.telsearch.tel.search.connector.PhoneDirectoryData;
@@ -12,6 +16,8 @@ import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 import ch.ivyteam.ivy.environment.AppFixture;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.telsearch.DirectoryMock;
 
 @IvyProcessTest
 public class PhoneDirectoryTest {
@@ -26,6 +32,18 @@ public class PhoneDirectoryTest {
     PhoneDirectoryData requestData = requestResult.data().last();
     assertThat(requestData.getMatches().size()).isEqualTo(8);
     assertThat(requestData.getMatches().get(0)).startsWith("Meier, John");
+  }
+
+  @Test
+  public void authKeyFeature(AppFixture fixture) {
+    fixture.environment("mock");
+    fixture.var("tel.search.api.key", "123_test");
+    Response response = Ivy.rest()
+            .client(UUID.fromString("20621516-9434-437b-8a8d-d41da2e7917b"))
+            .queryParam("was", "Müller")
+            .request().get();
+    assertThat(response.getStatus()).isEqualTo(200);
+    assertThat(response.getHeaderString(DirectoryMock.MY_KEY)).isEqualTo("123_test");
   }
 
 }
