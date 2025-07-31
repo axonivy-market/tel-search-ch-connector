f86058766b8b2bf26ab9b78ed57dbc0d0d393e25
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2025-07-31 14:32:43 +0700

    MARP-2597 End2end Multienvironment testing new workflow

--- tel-search-ch-connector-test/src_test/constant/TelSearchTestConstant.java
+++ tel-search-ch-connector-test/src_test/constant/TelSearchTestConstant.java
@@ -4,4 +4,6 @@ public class TelSearchTestConstant {
   public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
   public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
   public static final String API_KEY = "apiKey";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
